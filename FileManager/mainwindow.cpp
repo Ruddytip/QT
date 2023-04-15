3a3c9c5b858f02414b57b3481a7c16d47b5fa63c
Anton <Anton.Rudakov216@mail.ru>
2023-04-15 18:47:17 +0300

    update

---
+++ FileManager/mainwindow.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    fmqw = new FirstMyQtWidget(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete fmqw;
+    delete ui;
+}
+
